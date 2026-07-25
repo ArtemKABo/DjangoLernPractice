7966d3eaab3b1326aaa69d72d75e92c6a86d89ef
Артём Каримов <grandbo@list.ru>
2026-07-25 19:35:33 +0500

    "Валидация модели"

--- Tittles/views.py
+++ Tittles/views.py
@@ -4,8 +4,12 @@ from .models import User
 def index(request):
     
     if request.method == "POST":
-        user, created = User.objects.get_or_create(name = request.POST.get("name"))
-        data = {"person": user.name}
+        n = request.POST.get("name")
+        if n=="":
+            data ={"error": "Не указано имя пользователя", "person": ""}
+        else:
+            user, created = User.objects.get_or_create(name = n)
+            data = {"person": user.name}
         return TemplateResponse(request, "index.html",data)
     else: 
         data = {"person": ""}
